94007d913969832f8917db13666b316c4b734e9c
fengbo <1924144652@qq.com>
2024-05-25 02:11:25 +0800

    feat: add constant and nuxt.config.ts config

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = 'Nuxt3 App'
+export const appDesc = 'Nuxt3 Desc'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,59 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { appDesc } from "./constants"
+
 export default defineNuxtConfig({
-  devtools: { enabled: false },
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@unocss/nuxt',
+    '@nuxtjs/device',
+    '@nuxtjs/color-mode',
+    '@pinia-plugin-persistedstate/nuxt'
+  ],
+
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      link: [
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: appDesc },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ],
+    },
+  },
+
+  css: [],
+
+  devServer: {
+    host: '0.0.0.0'
+  },
+  
+  experimental: {
+    // when using generate, payload js assets included in sw precache manifest
+    // but missing on offline, disabling extraction it until fixed
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  piniaPersistedstate: {
+    cookieOptions: {
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+    }
+  },
+
+  colorMode: {
+    classSuffix: '-mode',
+  },
+
+  device: {
+    refreshOnResize: true
+  },
+
 })
